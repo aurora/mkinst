1b2fa615ee29f80313014039832f7f639bb1fd2f
Harald Lapp <harald@octris.org>
2018-10-12 12:57:49 +0200

    work on installer builder

--- build.sh
+++ build.sh
@@ -1,12 +1,112 @@
 #!/usr/bin/env bash
 
+# 
+# Tool for building self-contained installers.
+# Copyright (C) 2018 by Harald Lapp <harald@octris.org>
+#
+
+version="0.0.1"
+
 src="$1"
-dst="test.bin"
+dst=""
+custom=/dev/null
+
+function showusage {
+    echo "usage: $(basename $0) [OPTIONS] [--] <source> <target>
+    
+Creates a self-container installer from the specified input and stores it to
+the specified output.
+
+ARGUMENTS
+
+    <source>    Expects a file-name, a directory name or '-' for stdin. If the
+                name of a directory is specified, the tool will create a tar 
+                archive from it. If the source is no directory, a additional
+                custom installer script needs to be added using the option 
+
+    <target>    Either a file-name or '-' for stdout.
+
+OPTIONS
+
+-s, --script    Additional custom installer script This argument is required
+                if the source is either a file or stdin. This argument is 
+                discared if the source is a directory.
+-h, --help      Display this usage information.
+    --version   Show version and exit.
+"
+}
+
+
+while [[ "${1:0:1}" = "-" ]]; do
+    case $1 in
+        -)
+            break
+            ;;
+        --)
+            shift
+            break
+            ;;
+        -s|--script)
+            if [ -f "$2" ]; then
+                echo "Custom installer script does not exist."
+                exit 1
+            fi
+            
+            custom="$2"
+            shift
+            ;;
+        -h|-\?|--help)
+            showusage
+            exit 1
+            ;;
+        --version)
+            echo $version
+            exit 1
+            ;;
+    esac
+    
+    shift
+done
+
+if [ "$2" = "" ]; then
+    showusage
+    exit 1
+fi
+
+if [ "$1" = "-" ]; then
+    src=$(mktemp 2>/dev/null || mktemp -t "tmp.XXXXXXXXXX")
+    
+    trap 'rm -f ${src}; exit 1' HUP INT QUIT TERM
+    
+    cat - > $src
+elif [ -d "$1" ]; then
+    exit 255    # not implemented, yet
+
+    src=$(mktemp 2>/dev/null || mktemp -t "tmp.XXXXXXXXXX")
+
+    trap 'rm -f ${src}; exit 1' HUP INT QUIT TERM
+elif [ -f "$1" ]; then
+    src="$1"
+else 
+    echo "Unable to read from source."
+    exit 1
+fi
+
+if [ "$2" = "-" ]; then
+    dst=/dev/stdout
+elif [ -e "$2" ]; then
+    echo "Target already exists."
+    exit 1
+else
+    dst="$2"
+fi
 
 check=($(CMD_ENV=xpg4 cksum "$src"))
 line=$(($(grep -n "#""MARKER:INSTALLER" "$0" | head -n 1 | cut -d ":" -f 1) + 1))
 
-tail +$line "$0" | sed -e "s;%%SUM%%;${check[0]};g" -e "s;%%SIZE%%;${check[1]};g" | cat - $src > $dst
+tail +$line "$0" \
+    | sed -e "s;%%SUM%%;${check[0]};g" -e "s;%%SIZE%%;${check[1]};g" \
+    | cat - $custom <(echo "exit") <(echo "#MARKER:PAYLOAD") $src > $dst
 
 chmod a+x $dst
 
@@ -30,7 +130,3 @@ if [ ${scheck[0]} -ne ${pcheck[0]} ] || [ ${scheck[1]} -ne ${pcheck[1]} ]; then
     echo "aborting"
     exit 1
 fi
-
-exit
-
-#MARKER:PAYLOAD
